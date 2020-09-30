17f210ea28491f722e03fede3175408debac84d5
absharma2201 <sharmaabhinav123@gmail.com>
2020-10-01 02:30:26 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ import 'package:iwish_app/app/home_page.dart';
 import 'package:iwish_app/app/landing_page.dart';
 import 'package:provider/provider.dart';
 import 'package:iwish_app/CloudDatabase/auth_widget_builder.dart';
-/*
 
+/*
+hello
 void main()  {
   runApp(MaterialApp(
       home: iWishSplashScreen(), title: 'iWish App design',
@@ -21,19 +22,25 @@ void main()  {
 }
 */
 void main() => runApp(MyApp(
-  authServiceBuilder: (_) => FirebaseAuthService(),
-  databaseBuilder: (_, uid) => FirestoreDatabase(uid: uid),
-  storageBuilder: (_, uid) => StorageService(uid: uid),
-));
+      authServiceBuilder: (_) => FirebaseAuthService(),
+      databaseBuilder: (_, uid) => FirestoreDatabase(uid: uid),
+      storageBuilder: (_, uid) => StorageService(uid: uid),
+    ));
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key, this.authServiceBuilder, this.databaseBuilder, this.storageBuilder})
+  const MyApp(
+      {Key key,
+      this.authServiceBuilder,
+      this.databaseBuilder,
+      this.storageBuilder})
       : super(key: key);
   // Expose builders for 3rd party services at the root of the widget tree
   // This is useful when mocking services while testing
   final FirebaseAuthService Function(BuildContext context) authServiceBuilder;
-  final FirestoreDatabase Function(BuildContext context, String uid) databaseBuilder;
-  final StorageService Function(BuildContext context, String uid) storageBuilder;
+  final FirestoreDatabase Function(BuildContext context, String uid)
+      databaseBuilder;
+  final StorageService Function(BuildContext context, String uid)
+      storageBuilder;
 
   @override
   Widget build(BuildContext context) {
@@ -57,13 +64,14 @@ class MyApp extends StatelessWidget {
         builder: (context, userSnapshot) {
           return MaterialApp(
             theme: ThemeData(
-              scaffoldBackgroundColor: Color(0xFF2d3447),//Color(0xffE5E5E5),//Color(0xFFFFFFFF),
-              primaryColor: Color(0xFF2d3447),//Color(0xFFFF4700),
+              scaffoldBackgroundColor:
+                  Color(0xFF2d3447), //Color(0xffE5E5E5),//Color(0xFFFFFFFF),
+              primaryColor: Color(0xFF2d3447), //Color(0xFFFF4700),
             ),
             debugShowCheckedModeBanner: false,
             home: AuthWidget(
               userSnapshot: userSnapshot,
-              nonSignedInBuilder: (_) => LandingPage(),//SignInPageBuilder(),
+              nonSignedInBuilder: (_) => LandingPage(), //SignInPageBuilder(),
               signedInBuilder: (_) => HomePage(),
             ),
             onGenerateRoute: Router.onGenerateRoute,
